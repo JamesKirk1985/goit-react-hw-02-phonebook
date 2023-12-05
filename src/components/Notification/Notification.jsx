01110ba972fe790e0dbc5e6851c426b07dc775ee
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-12-05 02:51:25 +0200

    000-0

--- src/components/Notification/Notification.jsx
+++
@@ -1,5 +0,0 @@
-export const Notification = ({message}) => {
-    return <>
-        <p>{ message}</p>
-    </>
-}
--- src/components/Section/Section.jsx
+++
@@ -1,7 +0,0 @@
-
-export const Section = ({title, children}) => {
-    return <>
-        <h2>{title}</h2>        
-        {children}
-    </>
-}
